b288e1261487cc497bf9f4dc40d4ca16c4c4d9d2
ahamv <mhmimhmimhmi@naver.com>
2022-06-26 18:11:47 +0900

    서버.js파일추가

---
+++ server.js
@@ -0,0 +1,59 @@
+// server.js
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app); 
+var io = require('socket.io')(http);    
+var path = require('path');
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {  
+  res.render('chat');
+});
+
+var count=1;
+io.on('connection', function(socket){ 
+  	console.log('user connected: ', socket.id);  
+  	var name = "익명" + count++;                 
+	socket.name = name;
+  	io.to(socket.id).emit('create name', name);   
+    io.emit('new connect', name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	socket.on('disconnect', function(){ 
+        io.emit('exit', name);
+	  console.log('user disconnected: '+ socket.id + ' ' + socket.name);
+	});
+
+	socket.on('send message', function(name, text){ 
+        if(name != socket.name) {
+            io.emit('change_name', socket.name, name);
+            socket.name = name;
+        }
+		var msg = name + ' : ' + text;
+		socket.name = name;
+    	console.log(msg);
+    	io.emit('receive message', msg);
+	});
+	
+});
+
+http.listen(3000, function(){ 
+	console.log('server on..');
+});
