f2d8e83c2be7cfdea04c7b17704f23f9ca2a98b3
ahamv <mhmimhmimhmi@naver.com>
2022-06-26 18:24:27 +0900

    이름바꿔서 해보기

--- app.js
+++ app.js
@@ -1,41 +1,59 @@
-var createError = require('http-errors');
+// server.js
+
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
+var http = require('http').Server(app); 
+var io = require('socket.io')(http);    
+var path = require('path');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.set('views', './views');
+app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', (req, res) => {  
+  res.render('chat');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+var count=1;
+io.on('connection', function(socket){ 
+  	console.log('user connected: ', socket.id);  
+  	var name = "익명" + count++;                 
+	socket.name = name;
+  	io.to(socket.id).emit('create name', name);   
+    io.emit('new connect', name);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	socket.on('disconnect', function(){ 
+        io.emit('exit', name);
+	  console.log('user disconnected: '+ socket.id + ' ' + socket.name);
+	});
+
+	socket.on('send message', function(name, text){ 
+        if(name != socket.name) {
+            io.emit('change_name', socket.name, name);
+            socket.name = name;
+        }
+		var msg = name + ' : ' + text;
+		socket.name = name;
+    	console.log(msg);
+    	io.emit('receive message', msg);
+	});
+	
 });
 
-module.exports = app;
+http.listen(3000, function(){ 
+	console.log('server on..');
+});
